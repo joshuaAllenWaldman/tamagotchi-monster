f1dc279f24a9f41de42d9f21858d8129a2801852
joshuaWaldman <joshuaallenwaldman@gmail.com>
2020-12-21 15:32:28 -0800

    basic css is done for now. JS logic underway

--- scripts/app.js
+++ scripts/app.js
@@ -6,42 +6,46 @@
 const startGameBtn = document.getElementById('start-game');
 const aboutBtn = document.getElementById('about')
 
+//main game buttons
+const feedBtn = document.getElementById('feed');
+const playBtn = document.getElementById('play');
+const sleepBtn = document.getElementById('sleep')
+
 
 
 
 // Stat meters
 
-const hungerMeter = document.getElementById('hunger');
-const boredomMeter = document.getElementById('boredom');
-const tirednessMeter = document.getElementById('tiredness');
+let hungerMeter = document.getElementById('hunger');
+let boredomMeter = document.getElementById('boredom');
+let tirednessMeter = document.getElementById('tiredness');
 
 
 
 
 //This is the monster class that the user's creature will be created from. 
-
-
-
-
 class Monster {
     constructor(name, hungerLevel, tirednessLevel, boredomLevel){
         this.name = name;
         this.hungerLevel = hungerLevel;
         this.tirednessLevel = tirednessLevel;
         this.boredomLevel = boredomLevel;
-        this.age = age;
     }
 
-    rest() {
+    sleep() {
         this.tirednessLevel = 0;
-        //trigger sleep animation
+        tirednessMeter.value = 0;
+        console.log(`sleep was used ${player.tirednessLevel}`)
     }
     play () { 
         this.boredomLevel -= 5;
+        boredomMeter.value -= 5;
+
         //trigger play animation
     }
     eat () {
-        hungerLevel = 0;
+        this.hungerLevel = 0;
+        hungerMeter.value = 0;
     }
     age () {
         
@@ -50,3 +54,33 @@ class Monster {
 
 
 
+
+
+
+//Game timer
+function gameOn ( ){
+    const gameTimer = setInterval(function () {
+        player.hungerLevel += 6;
+        player.tirednessLevel += 5;
+        player.boredomLevel += 3;
+        hungerMeter.value = player.hungerLevel;
+        tirednessMeter.value += player.tirednessLevel;
+        boredomMeter.value += player.boredomLevel;
+        console.log(player)
+    }, 3000);
+}
+
+
+
+const player = new Monster('inputer', 0, 0, 0);
+
+gameOn(); 
+console.log(player);
+ 
+
+
+// EVENT LISTENERS
+
+feedBtn.addEventListener('click', player.eat);
+playBtn.addEventListener('click', player.play);
+sleepBtn.addEventListener('click', player.sleep)
