bae04ea16d3d258f7d80730d63ed7e914dd3933e
joshuaWaldman <joshuaallenwaldman@gmail.com>
2020-12-21 13:43:06 -0800

    Two web page files created for opening screen and gameplay

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////    BUTTONS       /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+//landing buttons
+const startGame = docunenb
+
+
+
+
+
+//This is the monster class that the user's creature will be created from. 
+
+
+
+
+class Monster {
+    constructor(name, hungerLevel, tirednessLevel, boredomLevel){
+        this.name = name;
+        this.hungerLevel = hungerLevel;
+        this.tirednessLevel = tirednessLevel;
+        this.boredomLevel = boredomLevel;
+        this.age = age;
+    }
+
+    rest() {
+        this.tirednessLevel = 0;
+        //trigger sleep animation
+    }
+    play () { 
+        this.boredomLevel -= 5;
+        //trigger play animation
+    }
+    eat () {
+        hungerLevel = 0;
+    }
+    age () {
+        
+    }
+} 
+
