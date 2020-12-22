c722134d076960adaa493667e120a0ce2acb6f96
joshuaWaldman <joshuaallenwaldman@gmail.com>
2020-12-22 14:16:02 -0800

    Most JS logic is set! Moving on to styling.

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,18 @@
 ///////////////////////////////////////////////////////////////////////////////
-///////////////////////////    BUTTONS       /////////////////////////////////////
+///////////////////////////    BUTTONS and Inputs     /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-//landing buttons
-const startGameBtn = document.getElementById('start-game');
-const aboutBtn = document.getElementById('about')
+//start buttons
+const startGameBtn = document.getElementById('start-game-button');
+const aboutBtn = document.getElementById('about-button')
 
 //main game buttons
 const feedBtn = document.getElementById('feed');
 const playBtn = document.getElementById('play');
-const sleepBtn = document.getElementById('sleep')
+const sleepBtn = document.getElementById('sleep');
 
-
-
-
+//game over button
+const tryAgainBtn = document.getElementById('try-again-button');
 
 
 // Stat meters
@@ -22,8 +21,18 @@ let hungerMeter = document.getElementById('hunger');
 let boredomMeter = document.getElementById('boredom');
 let tirednessMeter = document.getElementById('tiredness');
 
+// Get Player Input for Name
+let monsterName = ''
+function getMonsterName (){
 
+    monsterName = document.getElementById('user-name').value;
 
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////    Game Objects and Logic     /////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 //This is the monster class that the user's creature will be created from. 
 class Monster {
@@ -32,6 +41,7 @@ class Monster {
         this.hungerLevel = 0;
         this.tirednessLevel = 0;
         this.boredomLevel = 0;
+        this.age = 1;
     }
 
     sleep() {
@@ -55,28 +65,64 @@ class Monster {
    
 } 
 
-
-
-const player = new Monster('eventual input', );
-
+const player = new Monster(monsterName);
 
 
 
-
-//Game timer
-function gameOn ( ){
-    const gameTimer = setInterval(function () {
-        player.hungerLevel += 3;
-        player.tirednessLevel += 3;
-        player.boredomLevel += 4;
-        hungerMeter.value = player.hungerLevel;
-        tirednessMeter.value = player.tirednessLevel;
-        boredomMeter.value = player.boredomLevel;
-        console.log(player.hungerLevel, player.boredomLevel, player.tirednessLevel )
-    }, 1000);
+function startGame ( ){
+    gameOn();
+    getMonsterName();
+    $('.start-screen').css('display', 'none')
+    $('.game-on').css('display', 'block')
+    $('.game-over').css('display', 'none')
+    $('.username-display').text(monsterName)
 }
 
 
+
+
+
+
+
+
+
+
+//Game timer      if need STOP  look up clearInterval
+function gameOn ( ){
+    const gameTimer = setInterval(function () {
+        player.hungerLevel += 1;
+        player.tirednessLevel += 2;
+        player.boredomLevel += 3;
+        hungerMeter.value = player.hungerLevel;
+        tirednessMeter.value = player.tirednessLevel;
+        boredomMeter.value = player.boredomLevel;
+        if(player.boredomLevel > 50 || player.hungerLevel > 50 || player.tirednessLevel > 50){
+            $('.game-on').css('display', 'none');
+            $('.game-over').css('display', 'block');
+            clearInterval(gameTimer);  
+        }
+    }, 1000);
+    const aging = setInterval(function (){
+        player.age += 1;
+        $('#age-display').text(player.age)
+    }, 10000)
+
+    
+}
+
+function reset () {
+    player.tirednessLevel = 0;
+    player.boredomLevel = 0;
+    player.hungerLevel = 0;
+    player.age = 0;
+    hungerMeter.value = player.hungerLevel;
+    tirednessMeter.value = player.tirednessLevel;
+    boredomMeter.value = player.boredomLevel;
+    gameOn();
+    $('.start-screen').css('display', 'none');
+    $('.game-on').css('display', 'block');
+    $('.game-over').css('display', 'none');
+}
 
 
 // gameOn(); 
@@ -84,8 +130,14 @@ console.log(player);
  
  
 
-// EVENT LISTENERS
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////    Event Listeners    /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+startGameBtn.addEventListener('click', startGame)
 
 feedBtn.addEventListener('click', function () {player.eat()});
 playBtn.addEventListener('click', function () {player.play()});
 sleepBtn.addEventListener('click', function () {player.sleep()})
+
+tryAgainBtn.addEventListener('click', reset)
